ad3ea3ff0bb3c4ec340bcb1632a1c215a9cc9eee
Ravi Chugh <ravi.k.chugh@gmail.com>
2012-03-02 14:36:44 -0800

    tuple tests

---
+++ tests/djs/arrays/test06.js
@@ -0,0 +1,22 @@
+var sumTriple = function(x) /*:
+  [;Lx;] [[x:Ref(Lx)]]
+       / [Lx |-> (a:{(and (v::Arr(Int)) (packed v) (= (len v) 3))}, lArrayProto)]
+      -> Int / same */ {
+  return x[0] + x[1] + x[2];
+};
+
+var a = /*: lA Arr(Int) */ [10,20,30];
+var b = /*: lB Arr(Int) */ [10,20,30,40];
+
+/*: [;lA;] */ sumTriple(a);
+
+/// this version allows x to have _more_ than 3 elements
+var sumThree = function(x) /*:
+  [;Lx;] [[x:Ref(Lx)]]
+       / [Lx |-> (a:{(and (v::Arr(Int)) (packed v) (>= (len v) 3))}, lArrayProto)]
+      -> Int / same */ {
+  return x[0] + x[1] + x[2];
+};
+
+/*: [;lA;] */ sumThree(a);
+/*: [;lB;] */ sumThree(b);
---
+++ tests/djs/arrays/test07.js
@@ -0,0 +1,20 @@
+var sumTriple = function(x) /*:
+  [;Lx;] [[x:Ref(Lx)]] / [Lx |-> (a:<Int,Int,Int>, lArrayProto)]
+      -> Int / same */ {
+  return x[0] + x[1] + x[2];
+};
+
+var a = /*: lA */ [10,20,30];
+var b = /*: lB */ [10,20,30,40];
+
+/*: [;lA;] */ sumTriple(a);
+
+/// this version allows x to have _more_ than 3 elements
+var sumThree = function(x) /*:
+  [;Lx;] [[x:Ref(Lx)]] / [Lx |-> (a:<Int,Int,Int,...>, lArrayProto)]
+      -> Int / same */ {
+  return x[0] + x[1] + x[2];
+};
+
+/*: [;lA;] */ sumThree(a);
+/*: [;lB;] */ sumThree(b);
