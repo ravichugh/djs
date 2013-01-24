f11a562f62d7e721e3dca34a6b42c04f7f14e264
Panagiotis Vekris <pvekris@cs.ucsd.edu>
2013-01-23 19:59:39 -0800

    Advances in adsafe.js: parse_query still not woeking

--- tests/djs/ADsafe/adsafe.js
+++ tests/djs/ADsafe/adsafe.js
@@ -398,74 +398,92 @@ var adsafe = (function () {
     // all at once. Removal is required to avoid memory leakage on IE6 and IE7.
     
           //TODO: PV had to rename the argument name
-            walkTheDOM(node, function (node1)
-                /*: (node1: Ref(~lNode)) / (~lNode: frzn) -> Top / (~lNode: sameType) */
-                {
-                    if (node1.tagName) {
-                        node1['___ on ___'] = node1.change = null;
-                    }
-                },
-                //XXX: PV added third argument to match definition
-                true
-                );
+//            walkTheDOM(node, function (node1)
+//                /*: (node1: Ref(~lNode)) / (~lNode: frzn) -> Top / (~lNode: sameType) */
+//                {
+//                    if (node1.tagName) {
+//                        node1['___ on ___'] = node1.change = null;
+//                    }
+//                },
+//                //XXX: PV added third argument to match definition
+//                true
+//                );
         };
+
+        /*: (~lSelector : Dict     > lObjPro) */ "#weak"; 
+        /*: (~lMatch    : Arr(Str) > lArrPro) */ "#weak"; 
     
-    //
-    //    function parse_query(text, id) {
-    //
-    //// Convert a query string into an array of op/name/value selectors.
-    //// A query string is a sequence of triples wrapped in brackets; or names,
-    //// possibly prefixed by # . & > _, or :option, or * or /. A triple is a name,
-    //// and operator (one of [=, [!=, [*=, [~=, [|=, [$=, or [^=) and a value.
-    //
-    //// If the id parameter is supplied, then the name following # must have the
-    //// id as a prefix and must match the ADsafe rule for id: being all uppercase
-    //// letters and digits with one underbar.
-    //
-    //// A name must be all lower case and may contain digits, -, or _.
-    //
-    //        var match,          // A match array
-    //            query = [],     // The resulting query array
-    //            selector,
-    //            qx = id
-    //                ? /^\s*(?:([\*\/])|\[\s*([a-z][0-9a-z_\-]*)\s*(?:([!*~|$\^]?\=)\s*([0-9A-Za-z_\-*%&;.\/:!]+)\s*)?\]|#\s*([A-Z]+_[A-Z0-9]+)|:\s*([a-z]+)|([.&_>\+]?)\s*([a-z][0-9a-z\-]*))\s*/
-    //                : /^\s*(?:([\*\/])|\[\s*([a-z][0-9a-z_\-]*)\s*(?:([!*~|$\^]?\=)\s*([0-9A-Za-z_\-*%&;.\/:!]+)\s*)?\]|#\s*([\-A-Za-z0-9_]+)|:\s*([a-z]+)|([.&_>\+]?)\s*([a-z][0-9a-z\-]*))\s*/;
-    //
-    //// Loop over all of the selectors in the text.
-    //
-    //        do {
-    //
-    //// The qx teases the components of one selector out of the text, ignoring
-    //// whitespace.
-    //
-    ////          match[0]  the whole selector
-    ////          match[1]  * /
-    ////          match[2]  attribute name
-    ////          match[3]  = != *= ~= |= $= ^=
-    ////          match[4]  attribute value
-    ////          match[5]  # id
-    ////          match[6]  : option
-    ////          match[7]  . & _ > +
-    ////          match[8]      name
-    //
-    //            match = qx.exec(string_check(text));
-    //            if (!match) {
-    //                error("ADsafe: Bad query:" + text);
-    //            }
-    //
-    //// Make a selector object and stuff it in the query.
-    //
-    //            if (match[1]) {
-    //
-    //// The selector is * or /
-    //
-    //                selector = {
-    //                    op: match[1]
-    //                };
-    //            } else if (match[2]) {
-    //
+        var parse_query = function (text, id)
+        /*: [;L;] (text: Str, id: Str) / () -> Ref(L) / (L : Arr(NotUndef)) */ 
+        {
+    
+    // Convert a query string into an array of op/name/value selectors.
+    // A query string is a sequence of triples wrapped in brackets; or names,
+    // possibly prefixed by # . & > _, or :option, or * or /. A triple is a name,
+    // and operator (one of [=, [!=, [*=, [~=, [|=, [$=, or [^=) and a value.
+    
+    // If the id parameter is supplied, then the name following # must have the
+    // id as a prefix and must match the ADsafe rule for id: being all uppercase
+    // letters and digits with one underbar.
+    
+    // A name must be all lower case and may contain digits, -, or _.
+    
+            var match     /*: Ref(~lMatch) */     =           null,     // A match array  //XXX: PV: added "null"
+                query                             = /*: L */  [],       // The resulting query array
+                selector  /*: Ref(~lSelector) */  =           null;     //XXX: PV: added "null"
+
+                //TODO: Exception Failure: convert CRegExp
+                //,qx = id
+                //    ? /^\s*(?:([\*\/])|\[\s*([a-z][0-9a-z_\-]*)\s*(?:([!*~|$\^]?\=)\s*([0-9A-Za-z_\-*%&;.\/:!]+)\s*)?\]|#\s*([A-Z]+_[A-Z0-9]+)|:\s*([a-z]+)|([.&_>\+]?)\s*([a-z][0-9a-z\-]*))\s*/
+                //    : /^\s*(?:([\*\/])|\[\s*([a-z][0-9a-z_\-]*)\s*(?:([!*~|$\^]?\=)\s*([0-9A-Za-z_\-*%&;.\/:!]+)\s*)?\]|#\s*([\-A-Za-z0-9_]+)|:\s*([a-z]+)|([.&_>\+]?)\s*([a-z][0-9a-z\-]*))\s*/;
+    
+    // Loop over all of the selectors in the text.
+    
+            /*: ( &text: Str, &query: Ref(L), L: Arr(NotUndef)) -> ( &text: sameType, &query: sameType, L: sameType) */ 
+
+            do {
+    
+    // The qx teases the components of one selector out of the text, ignoring
+    // whitespace.
+    
+    //          match[0]  the whole selector
+    //          match[1]  * /
+    //          match[2]  attribute name
+    //          match[3]  = != *= ~= |= $= ^=
+    //          match[4]  attribute value
+    //          match[5]  # id
+    //          match[6]  : option
+    //          match[7]  . & _ > +
+    //          match[8]      name
+    
+                //TODO: Fix with regex support
+                //match = qx.exec(string_check(text));
+
+                /*: match lMatch */ "#thaw";
+                match = ["a", "b", "c"];    //PV: temporarily using this 
+                //if (!match) {
+                //    error("ADsafe: Bad query:" + text);
+                //}
+                /*: match (~lMatch, thwd lMatch) */ "#freeze";
+    
+    // Make a selector object and stuff it in the query.
+    
+//                if (match[1]) {
+    
+    // The selector is * or /
+    
+                    /*: selector lSelector */ "#thaw";
+                    selector = {
+                        op: "a"// match[1]
+                    };
+                    /*: selector (~lSelector, thwd lSelector) */ "#freeze";
+//                } 
+                
+    //TODO: resuse after "if-then-else" type optimization
+    //            else if (match[2]) {
+    
     //// The selector is in brackets.
-    //
+    
     //                selector = match[3] ? {
     //                    op: '[' + match[3],
     //                    name: match[2],
@@ -475,9 +493,9 @@ var adsafe = (function () {
     //                    name: match[2]
     //                };
     //            } else if (match[5]) {
-    //
+    
     //// The selector is an id.
-    //
+    
     //                if (query.length > 0 || match[5].length <= id.length ||
     //                        match[5].slice(0, id.length) !== id) {
     //                    error("ADsafe: Bad query: " + text);
@@ -486,33 +504,33 @@ var adsafe = (function () {
     //                    op: '#',
     //                    name: match[5]
     //                };
-    //
+    
     //// The selector is a colon.
-    //
+    
     //            } else if (match[6]) {
     //                selector = {
     //                    op: ':' + match[6]
     //                };
-    //
+    
     //// The selector is one of > + . & _ or a naked tag name
-    //
+    
     //            } else {
     //                selector = {
     //                    op: match[7],
     //                    name: match[8]
     //                };
     //            }
-    //
-    //// Add the selector to the query.
-    //
-    //            query.push(selector);
-    //
-    //// Remove the selector from the text. If there is more text, have another go.
-    //
-    //            text = text.slice(match[0].length);
-    //        } while (text);
-    //        return query;
-    //    }
+    
+    // Add the selector to the query.
+    
+                //query.push(selector);
+    
+    // Remove the selector from the text. If there is more text, have another go.
+    
+                //text = text.slice(match[0].length);
+            } while (text);
+            return query;
+        };
     //
     //
     //    hunter = {
