de66da75896aec9acd4826303e5555ac59269c63
Panagiotis Vekris <pvekris@cs.ucsd.edu>
2013-01-23 18:23:09 -0800

    Adding example with array weak location

---
+++ tests/djs/panos/simple/weak00.js
@@ -0,0 +1,22 @@
+/*: (~lA : Dict > lObjPro) */ "#weak";
+/*: (~lMatch    : Arr(Str) > lArrPro) */ "#weak"; 
+
+var t = "aa";
+var a /*: Ref(~lA) */ = null; 
+var match /*: Ref(~lMatch) */ = null;
+
+/*: (&t: t0: Str) -> 
+    (&t: t1: Str) */ 
+while(t) {
+
+  /*: a lA */ "#thaw";
+  a = { op: "sss" }; 
+  /*: a (~lA, thwd lA) */ "#freeze";
+
+  /*: match lMatch */ "#thaw";
+  match = ["a", "b", "c"];    //PV: temporarily using this 
+  /*: match (~lMatch, thwd lMatch) */ "#freeze";
+  
+  t = t.slice(1);
+
+} ;
