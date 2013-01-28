a2d71e3eff6ad9ebd2922cd93fc29c8b82adf1f2
Panagiotis Vekris <pvekris@cs.ucsd.edu>
2013-01-28 11:45:19 -0800

    Switch statement issue

---
+++ tests/djs/panos/simple/switch00.js
@@ -0,0 +1,33 @@
+
+/*: "tests/djs/ADsafe/__dom.dref" */ "#use";
+
+var document /*: Ref(~lDocument) */ = null;
+
+var quest = function(query, selector) 
+/*: [; L;] (Ref(L), Ref(~lSelector)) / (L: { Arr(Ref(~lSelector)) | (packed v) } > lArrPro) -> Int / sameType */ 
+{
+  var i /*: Int */  = 0;
+
+  switch (selector.op) {
+    case "one":
+           i = 1;
+           break;
+    case "two":
+           i = 2;
+           break;
+    default:
+           i = 3;
+  }
+
+//  //PV: The following works
+//  if (selector.op == "one") {
+//    i = 1;
+//  }
+//  else if (selector.op == "two") {
+//    i = 2;
+//  }
+//  else {
+//    i = 3;
+//  }
+  return i;
+};
