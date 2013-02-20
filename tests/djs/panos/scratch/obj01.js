96e343dc701e5a3d2710cd46748699228367c921
Ravi Chugh <ravi.k.chugh@gmail.com>
2013-02-19 16:36:04 -0800

    playing with "function" tag and dict domain

--- tests/djs/panos/scratch/obj01.js
+++ tests/djs/panos/scratch/obj01.js
@@ -1,13 +1,19 @@
 var foo = function(s, h) 
-  /*: (s:Str, h: Ref) / (h: { f: () -> Top, _: Bot}  > lObjPro)-> Top  / sameType */ 
+  /*: (s:Str, h: Ref)
+    / (h: {Dict|(and (dom v {"f"})
+                     ((sel v "f") :: () -> Top)
+                     (= (tag (sel v "f")) "function"))} > lObjPro,
+       lObjPro: {_:Bot} > lROOT)
+   -> Top / sameType */ 
 {
   var func = h[s];
 
   if (typeof func  === 'function') {
+    assert (s == "f");
     assert(/*: {(= (tag v) "function")}  */  (func));
     assert(/*: () -> Top  */  (func));
 
-    func();
+    //func();
   }
 
 };
