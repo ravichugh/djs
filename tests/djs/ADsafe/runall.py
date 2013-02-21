df973f13d080364c9ee300283f2bf2714db53923
Panagiotis Vekris <pvekris@cs.ucsd.edu>
2013-02-20 23:22:18 -0800

    Adsafe.js: adding TODO count in runall script.

--- tests/djs/ADsafe/runall.py
+++ tests/djs/ADsafe/runall.py
@@ -1,47 +1,49 @@
 #! /usr/bin/env python
 import re
 import time
+import string
 from multiprocessing import Process, Pipe, Queue
 from Queue import Empty
+from collections import Counter
 
 from optparse import OptionParser
-
+# filename, timeout for z3
 files = [    
-  "00-adsafe.js",
-  "01-error.js",
-  "02-string_check.js",
-  "03-owns.js",
-  "04-reject_name.js",
-  "05-reject_property.js",
-  "06-reject_global.js",
-  "07-getStyleObject.js",
-  "08-walkTheDOM.js",
-  "09-purge_event_handlers.js",
-  "10-parse_query.js",
-  "11-hunter.js",
-  "12-pecker.js",
-  "13-quest.js",
-  "14-make_root.js",
-  "15-append.js",
-  "16-blur-check-class.js",
-  "17-clone-empty.js",
-  "18-enable-ephemeral-explode.js",
-  "19-fire.js",
-  "20-focus-fragment.js",
-  "21-get.js",
-  "22-klass-mark-off-on.js",
-  "23-protect.js",
-  "24-replace.js",
-  "25-select.js",
-  "26-style.js",
-  "27-tag-text-title.js",
-  "28-value.js",
-  "29-dom.js",
-  "30-the_event.js",
-  "31-adsafe_create-get.js",
-  "32-adsafe_go.js",
-  "33-adsafe-has-later.js",
-  "34-adsafe-lib-intercept.js"
+ ("00-adsafe.js", 0),
+ ("01-error.js", 0),
+ ("02-string_check.js", 0),
+ ("03-owns.js", 0),
+ ("04-reject_name.js", 0),
+ ("05-reject_property.js", 0),
+ ("06-reject_global.js", 0),
+ ("07-getStyleObject.js", 0),
+ ("08-walkTheDOM.js", 0),
+ ("09-purge_event_handlers.js", 0),
+ ("10-parse_query.js", 10),
+ ("11-hunter.js", 0),
+ ("12-pecker.js", 0),
+ ("13-quest.js", 0),
+ ("14-make_root.js", 0),
+ ("15-append.js", 0),
+ ("16-blur-check-class.js", 0),
+ ("17-clone-empty.js", 0),
+ ("18-enable-ephemeral-explode.js", 0),
+ ("19-fire.js", 1),
+ ("20-focus-fragment.js", 500),
+ ("21-get.js", 0),
+ ("22-klass-mark-off-on.js", 0),
+ ("23-protect.js", 0),
+ ("24-replace.js", 0),
+ ("25-select.js", 0),
+ ("26-style.js", 0),
+ ("27-tag-text-title.js", 0),
+ ("28-value.js", 0),
+ ("29-dom.js", 0),
+ ("30-the_event.js", 0),
+ ("31-adsafe_create-get.js", 0),
+ ("32-adsafe_go.js", 0),
+ ("33-adsafe-has-later.js", 0),
+ ("34-adsafe-lib-intercept.js", 0)
   ]
 
 parser = OptionParser(usage='usage: %prog [options] ')
@@ -64,7 +66,7 @@ parser.add_option(  "-t", "--timeout",
 timeout = int(options.timeout)
 
 
-com = ["../../../src/system-dref", "-djs"]
+com = ["../../../src/system-dref"]
 
 class bcolors:
   HEADER = '\033[95m'
@@ -88,48 +90,54 @@ class bcolors:
 def timeout_command(command, q):
   import subprocess, datetime, os, time, signal
   process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-  q.put(process.stdout.read())
+  q.put(process.stdout.read() + process.stderr.read())    #just append stdout and stderr
 
 def command(command):
   import subprocess, datetime, os, time, signal
   process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-  return process.stdout.read()
+  return process.stdout.read() + process.stderr.read()
 
 file_range = range(int(options.file_start), int(options.file_stop))
 
 reOK = re.compile(r'OK! (\d*)')
 reFail = re.compile(r'TC ERROR!')
 rePError = re.compile(r'PARSE ERROR!')
+reFError = re.compile(r'Fatal error')
 tot_queries = 0
 tot_time = 0
 
 
-def process(output, elapsed_time):
+def process(fname, output, elapsed_time):
   global tot_queries
+  todos = string.count(open(fname).read(), "var")
   if output:
     matchOK = reOK.search(output)
     if matchOK:
       groupOK = matchOK.group
       q = int(groupOK(1))
       tot_queries = tot_queries + q
-      print  "%30s (ET: %7.3f sec) " % (f, elapsed_time) + bcolors.OKGREEN + "OK! %s queries" % q + bcolors.ENDC
+      print  "%30s (ET: %7.3f sec, #TODOS: %2d) " % (f, elapsed_time, todos) + bcolors.OKGREEN + "OK! %s queries" % q + bcolors.ENDC
     
     matchFail = reFail.search(output)
     if matchFail:
-      print "%30s (ET: %7.3f sec) " % (f, elapsed_time) + bcolors.FAIL + "TC Fail" + bcolors.ENDC
+      print "%30s (ET: %7.3f sec, #TODOS: %2d) " % (f, elapsed_time, todos) + bcolors.FAIL + "TC Fail" + bcolors.ENDC
 
     matchPError = rePError.search(output)
     if matchPError:
-      print "%30s (ET: %7.3f sec) " % (f, elapsed_time) + bcolors.FAIL + "Parse Error" + bcolors.ENDC
+      print "%30s (ET: %7.3f sec, #TODOS: %2d) " % (f, elapsed_time, todos) + bcolors.FAIL + "Parse Error" + bcolors.ENDC
+    
+    matchFError = reFError.search(output)
+    if matchFError:
+      print "%30s (ET: %7.3f sec, #TODOS: %2d) " % (f, elapsed_time, todos) + bcolors.FAIL + "Fatal Error" + bcolors.ENDC
   else:
-    print "%30s (ET: %7.3f sec) " % (f, elapsed_time) + bcolors.WARNING + "Timed out" + bcolors.ENDC
+    print "%30s (ET: %7.3f sec, #TODOS: %2d) " % (f, elapsed_time, todos) + bcolors.WARNING + "Timed out" + bcolors.ENDC
 
 
 for i in file_range:
-  f = files[i]
+  (f,t) = files[i]
   result_queue = Queue(1)
   if timeout > 0:
-    p = Process(target=timeout_command, args=(com + [f], result_queue, ))
+    p = Process(target=timeout_command, args=(com + ["-timeout", str(t)] + ["-djs", f], result_queue, ))
     start_time = time.time()
     p.start()
     try:
@@ -142,11 +150,11 @@ for i in file_range:
       output = None
   else:
     start_time = time.time()
-    output = command(com + [f])
+    output = command(com + ["-timeout", str(t)] + ["-djs", f])
 
   elapsed_time = float(time.time() - start_time)
   tot_time = tot_time + elapsed_time
-  process(output, elapsed_time)
+  process(f, output, elapsed_time)
 
 print "-------------------------------------------------------------"
 print  bcolors.OKGREEN + "Total Time: %7.3f sec, Total queries: %d" % (tot_time, tot_queries) + bcolors.ENDC
