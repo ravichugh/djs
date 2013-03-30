ce84888c537ea3076b7e5dd3bbaf383aa365617f
Panagiotis Vekris <pvekris@cs.ucsd.edu>
2013-03-29 20:30:06 -0700

    Adding stats script

--- tests/djs/mozilla/unzip.py
+++ tests/djs/mozilla/unzip.py
@@ -12,15 +12,25 @@ home_url =  "https://addons.mozilla.org/"
 us_ff_url = home_url + "en-US/firefox/"
 
 
-def unzip_file(path):
-  addon_root = os.path.splitext(path)[0]
+def unzip_file(archive):
+  addon_root = os.path.splitext(archive)[0]
   if not os.path.exists(addon_root):
     os.makedirs(addon_root)
-  
-  #if zipfile.is_zipfile(path):
-
-  
-
+ 
+  # If the direcrory is not empty ...
+  if not os.listdir(addon_root):
+    if zipfile.is_zipfile(archive):
+      zf = zipfile.ZipFile(archive)
+      print("Extracting: " + archive + " ...")
+      # Extract to the addon root directory
+      try:
+        zf.extractall(addon_root)
+        open(os.path.join(addon_root,"SUCCESS"), 'w').close()
+      except zipfile.BadZipfile:
+        open(os.path.join(addon_root,"FAILED_EXTRACTION"), 'w').close()
+  else: 
+    open(os.path.join(addon_root,"SUCCESS"), 'w').close()
+    print("Non-empty root dir: " + addon_root + ". Assuming extracted.")
 
 
 # Unzip all files with the ending 'unzip_type'
