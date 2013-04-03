9be9461bb8f7045253a58b7cf55d2d9c78a8180a
Panagiotis Vekris <pvekris@cs.ucsd.edu>
2013-04-02 18:51:24 -0700

    Small change in example to not have to include the .dref files

--- tests/djs/panos/scratch/func10.js
+++ tests/djs/panos/scratch/func10.js
@@ -1,16 +1,17 @@
 //TODO: TC this
 
-/*: "tests/djs/mozilla-djs/rapidshare-downloadhelper/__mozilla.dref" */ "#use";
-/*: "tests/djs/mozilla-djs/rapidshare-downloadhelper/__dom.dref" */ "#use";
+/*: (~lWindow: {
+    setTimeout: (this:Ref(~lWindow), (this:Top)  -> Top , Num) -> Top,
+    setInterval: (this: Top, (this: Top) / (~lWindow: frzn) -> Top / sameType, Num) -> Int,    
+    clearInterval: (this:Top, Num) -> Top, 
+    document: Ref(~lNode)
+  } > lObjPro)
+*/ "#weak";
 
-var window /*: Ref(~lWindow) */ = "#extern";
+var window = /*: Ref(~lWindow) */ "#extern";
 
-var foo = function(doc) /*: (Ref(~lDocument)) -> Top */ {
-
-  var ina  = window.setInterval(
-      function() /*: (this: Top) -> Top */ {
-        var zeit = doc.getElementById("zeit");
-        window.clearInterval(ina);
-      },
-      1000);
-};
+var ina  = window.setInterval(
+    function() /*: (this: Top) -> Top */ {
+      window.clearInterval(ina);
+    },
+    1000);
