7b83b4ec0a5c3a8daea3a4610fb0291daa9b26e5
Panagiotis Vekris <pvekris@cs.ucsd.edu>
2013-04-30 15:17:22 -0700

    Done with 17

--- tests/djs/ADsafe/17-clone-empty.js
+++ tests/djs/ADsafe/17-clone-empty.js
@@ -1,3 +1,6 @@
+
+var int_to_string = /*: (Int) -> Str */ "#extern";
+
 var error = /*: {( and (v::(Str) -> { FLS }) (v:: () -> { FLS }))} */ "#extern";
 
 /*: "tests/djs/ADsafe/__dom.dref" */ "#use";
@@ -12,33 +15,8 @@ var reject_global =
     )} */ "#extern";
 
 
-/*: tyNode  {
-    getComputedStyle     : (this: Ref(~htmlElt), node : Ref(~htmlElt), str  : Str) -> Ref(~lStyle),
-    currentStyle         : Ref(~lStyle),
-    firstChild           : Ref(~htmlElt),
-    nextSibling          : Ref(~htmlElt),
-    parentNode           : Ref(~htmlElt),
-    childNodes           : Ref(~htmlElts),
-    change               : Ref(~lEvent),
-    "___ on ___"         : Ref(~lEvent),
-    "___adsafe root___"  : Bool,
-    tagName              : Str,
-    className            : Str,
-    name                 : Str,
-    nodeName             : Str,
-    nodeValue            : Str,
-    disabled             : Bool,
-    checked              : Bool,
-    fire                 : (NotUndef) -> Top,
-    blur                 : (this: Ref(~htmlElt)) -> Top,
-    cloneNode             : (this: Ref(~htmlElt), deep:Bool) -> Ref(~htmlElt),
-    getElementsByTagName : [;L;] (this: Ref(~htmlElt), name : Str) / () -> Ref(L) / (L: {Arr(Ref(~htmlElt))|(packed v)} > lArrPro)
-  } > lObjPro */ "#define";
-
-
 var star    /*: Bool */         = "#extern";
 var purge_event_handlers = /*: (node: Ref(~htmlElt)) -> Top */ "#extern";
-var int_to_string /*: (Int) -> Str */ = "#extern";
 
 // A Bunch is a container that holds zero or more dom nodes.
 // It has many useful methods.
@@ -62,10 +40,8 @@ var clone =  function (deep, n)
 /*: (this: Ref(~lBunch), deep:Bool, n: {Int|(>= v 0)}) -> 
   {(ite (truthy n) (v::Ref(~lBunches)) (v::Ref(~lBunch)))} */
 {
-  /*: this lBunch */ "#thaw";
   var a = /*: lA {Arr(Ref(~lBunch))|(and (packed v) (= (len v) 0))} */ [];
   var b = this.___nodes___;    
-  /*: this (~lBunch, thwd lBunch) */ "#freeze";
 
   var i /*: { Int | (>= v 0)} */ = 0,
       j /*: { Int | (>= v 0)} */ = 0,
@@ -155,46 +131,34 @@ var each = function (func)
 var value = /*: lArr { Arr(Str) | (packed v)} */ [];
 
 var empty = function () 
-//TODO: TCing succeeds for the two cases separately but not in the inconsistent 
-//part and not fot the intersection type.
-/* {(and
+/*: {(and
     (v :: (this: Ref(~lBunch)) / (&value: Ref(lArr), lArr: { Arr(Str) | (packed v) }  > lArrPro) -> Ref(~lBunch) / sameType)
-    (v :: (this: Ref(~lBunch)) / (&value: Ref(lObj), lObj: { }  > lObjPro) -> Ref(~lBunch) / sameType)
+    (v :: (this: Ref(~lBunch)) / (&value: Ref(lObj), lObj: {                       }  > lObjPro) -> Ref(~lBunch) / sameType)
     )} */
-/* (this: Ref(~lBunch)) / (&value: Ref(lArr), lArr: { Arr(Str) | (packed v) }  > lArrPro) -> Ref(~lBunch) / sameType */
-/*: (this: Ref(~lBunch)) / (&value: Ref(lObj), lObj: { }  > lObjPro) -> Ref(~lBunch) / sameType */
+    
 {
   reject_global(this);
-  /*: this lBunch */ "#thaw";
   var b = this.___nodes___;
-  /*: this (~lBunch, thwd lBunch) */ "#freeze";
   var i /*: { Int | (>= v 0)} */ = 0,
       node /*: Ref(~htmlElt) */ = null;
 
-  //PV: ugly hack to allow b.length as param to int_to_string
-  var tmp_bl;
-
   if (isArray(value)) {
-//    /*: b htmlElts */ "#thaw";
-//    tmp_bl = b.length;
-//    /*: b (~htmlElts, thwd htmlElts) */ "#freeze";
-//    if (value.length !== tmp_bl) {
-//      error('ADsafe: Array length: ' + int_to_string(tmp_bl) + '-' +
-//          int_to_string(value.length));
-//    }
-//    assert(/*: Ref(~htmlElts) */ (b));
-//
-//    /*: b htmlElts */ "#thaw";
-//    assume(b != null);
-//    /*: (&b: Ref(htmlElts), htmlElts: {Arr(Ref(~htmlElt)) | (packed v)} > lArrPro) -> sameExact */
-//    for (i = 0; i < b.length; i += 1) {
-//      node = b[i];
-//      while (node.firstChild) {
-//        purge_event_handlers(node);
-//        node.removeChild(node.firstChild);
-//      }
-//    }
-//    /*: b (~htmlElts, thwd htmlElts) */ "#freeze";
+
+    /*: b htmlElts */ "#thaw";
+    if (value.length !== b.length) {
+      error('ADsafe: Array length: ' + int_to_string(b.length) + '-' +
+          int_to_string(value.length));
+    }
+
+    /*: (&b: Ref(htmlElts), htmlElts: {Arr(Ref(~htmlElt)) | (packed v)} > lArrPro) -> sameExact */
+    for (i = 0; i < b.length; i += 1) {
+      node = b[i];
+      while (node.firstChild) {
+        purge_event_handlers(node);
+        node.removeChild(node.firstChild);
+      }
+    }
+    /*: b (~htmlElts, thwd htmlElts) */ "#freeze";
   }
   else {
     /*: b htmlElts */ "#thaw";
